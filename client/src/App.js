ea027e5e0d3b091c4ac296c1b8957d6d3e39e4da
schadokar <shubhamchadokar04@gmail.com>
2019-10-23 09:05:29 +0530

    m: initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Container } from "semantic-ui-react";
+
+function App() {
+  return (
+    <div>
+      <Container>Import components here</Container>
+    </div>
+  );
+}
+
+export default App;
